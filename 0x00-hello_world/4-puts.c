c2e58034721da604fb6dca2902109d215d676d2d
426902 <suleimanhabeeb16@gmail.com>
2022-07-04 09:29:43 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 /**
+ * main -entry point
  *
- * * main -entry point
- *
- * * Return: Always 0 (sucess)
- *
- * */
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
