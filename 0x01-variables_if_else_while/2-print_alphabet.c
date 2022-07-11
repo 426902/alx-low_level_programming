c8a57cda96d9cf3374006e441e63622ca6e438cc
426902 <suleimanhabeeb16@gmail.com>
2022-07-11 03:29:53 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- entry point
+ * Description: alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n')';
+	return (0);
+}
