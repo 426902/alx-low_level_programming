3deb153fc8b4677c075bfa934bda79f5cef733cd
426902 <suleimanhabeeb16@gmail.com>
2022-07-12 03:50:47 -0700

    nestedloop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 
 	unsigned long x, y, z;
 
-	x = o;
+	x = 0;
 	y = 1;
 	for (count = 1; count <= 49; count++)
 	{
 		z = x + y;
 		x = y;
-		y + z;
+		y = z;
 		printf("%lu, ", z);
 	}
 	printf("%lu\n", z + x);
