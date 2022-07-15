0ad8564aec451191d6596e1a17c9d28bfc11be37
426902 <suleimanhabeeb16@gmail.com>
2022-07-15 01:16:35 -0700

    101 more chaeck

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -29,7 +29,7 @@ void print_number(int n)
 			aux = aux * 10;
 			num = num / 10;
 		}
-		_putchar((num2 / aux) +'0');
+		_putchar((num2 / aux) + '0');
 		aux = aux / 10;
 		for (i = aux; i >= 1; i = i / 10)
 			_putchar((num2 / i) % 10 + '0');
