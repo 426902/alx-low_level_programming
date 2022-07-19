b217065a37f5ddcc4f7f5e684bab6d88a074a0c3
426902 <suleimanhabeeb16@gmail.com>
2022-07-19 01:19:52 -0700

    5strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,22 +11,24 @@ nclude "main.h"
 
 char *_strstr(char *haystack, char *needle)
 {
-int index;
+	char *result = haystack, *fneedle = needle;
 
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {
-if (needle[index + 1] == '\0')
-return (haystack);
-index++;
-} while (haystack[index] == needle[index]);
-}
-haystack++;
-}
-return ('\0');
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
 }
