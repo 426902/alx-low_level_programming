b0a061a128647729096aea194701aecb2a5b060d
426902 <suleimanhabeeb16@gmail.com>
2022-07-18 20:14:04 -0700

    0x07 fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,28 +10,23 @@ nclude "main.h"
  */
 
 char *_strstr(char *haystack, char *needle)
-
 {
+int index;
 
-	int index;
-
-	if (*needle == 0)
-
-		return (haystack);
-
-	while (*haystack)
-	{
-		index = 0;
-
-		if (haystack[index] == needle[index])
-		{
-			do {
-				if (needle[index + 1] == '\0')
-					return (haystack);
-				index++;
-			} while (haystack[index] == needle[index]);
-		}
-		haystack++;
-	}
-	return ('\0');
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+index = 0;
+if (haystack[index] == needle[index])
+{
+do {
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+} while (haystack[index] == needle[index]);
+}
+haystack++;
+}
+return ('\0');
 }
