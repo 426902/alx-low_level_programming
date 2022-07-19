cec963ddc81d3a7cf79e7d9135a0dedffbe8051a
426902 <suleimanhabeeb16@gmail.com>
2022-07-18 20:02:21 -0700

    1-str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,21 +9,14 @@
  */
 
 mainar *_strchr(char *s, char c)
-	
 {
-	
 	int index;
-	
+
 	for (index = 0; s[index] >= '\0'; index++)
-		
+
 	{
-		
-		if (s[index] == c)
-			
-			return (s + index);
-		
+	if (s[index] == c)
+	return (s + index);	
 	}
-	
 	return ('\0');
-	
 }
