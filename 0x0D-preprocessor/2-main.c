928ee34ef4c8c759b0a2c1e358e41b891a5c31eb
426902 <suleimanhabeeb16@gmail.com>
2022-08-02 01:46:34 -0700

    2 main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print name of file it was compile from
- * Return: 0
+ * main - check the code for Holberton School students.
+ * Return: always 0
  */
 
 int main(void)
